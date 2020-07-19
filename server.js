94356c4615957d24126ae1d09ac8fff1aae22465
Zhifeng <tzephons@gmail.com>
2020-07-20 01:10:37 +0200

    Ng build --prod

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/client-angular'));
+ngApp.use(express.static('./dist/Client-Angular'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/client-angular/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/Client-Angular/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
