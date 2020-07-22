6c94289d1d5ddb4fa02f5a3bc2c3071869679c91
Zhifeng <tzephons@gmail.com>
2020-07-22 09:47:53 +0200

    Showing full name on analysis page.

--- src/app/core/model/analysis.ts
+++ src/app/core/model/analysis.ts
@@ -1,10 +1,12 @@
 export class Analysis {
+    public userfullname: string;
     public username: string;
     public tweet_text: string;
     public created_at: Date;
     public sentiment: Object;
 
     constructor() {
+        this.userfullname = '';
         this.username = '';
         this.tweet_text = '';
         this.created_at = null;
